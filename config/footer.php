00c4caddcb380c8b448e112c723518b43edbdc9d
DennisGambini <dennis.gambini@gmail.com>
2022-06-10 16:49:26 +0200

    bozza footer con dati dinamici da config

---
+++ config/footer.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    [
+        'name' => 'DC COMICS',
+        'content' => [
+            'Characters', 
+            'Comics', 
+            'Movies', 
+            'TV', 
+            'Games', 
+            'Videos', 
+            'News'
+        ]
+    ],
+    [
+        'name' => 'SHOP',
+        'content' => [
+            'Shop DC', 
+            'Shop DC Collectibles'
+        ]
+    ],
+    [
+        'name' => 'DC',
+        'content' => [
+            'Terms Of Use', 
+            'Privacy Policy (New)', 
+            'Ad Choices', 
+            'Advertising', 
+            'Jobs', 
+            'Subscriptions', 
+            'Talent Workshops',
+            'CPSC Certificates',
+            'Ratings',
+            'Shop Help',
+            'Contact Us'
+        ]
+    ],
+    [
+        'name' => 'SITES',
+        'content' => [
+            'DC', 
+            'MAD Magazine', 
+            'DC Kids', 
+            'DC Universe', 
+            'DC Power Visa'
+        ]
+    ],
+]
+
+?>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,30 @@
+<?php
+$data = config('footer');
+?>
+
 <footer>
-    sono il footer
+
+    <div class="footer-top">
+        <div class="container">
+            footer top
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="liste">
+                @foreach ($data as $list)
+                    <ul>
+                        <li class="head">{{$list['name']}}</li>
+                        @foreach($list['content'] as $li)
+                        <li class="normal">
+                            <a href="#">{{$li}}</a>
+                        </li>
+                        @endforeach
+                    </ul>
+                @endforeach
+            </div>
+        </div>
+    </div>
+
 </footer>
